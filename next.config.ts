832b0d2a0be27abf37b1dbd99db461159db21ce7
Hemerson Vianna <hemerson.lourenco@gmail.com>
2026-09-02 20:28:47 -0300

    feat: initial structure

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  basePath: "/bof-rj",
+  assetPrefix: "/bof-rj",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
